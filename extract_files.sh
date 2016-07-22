e789f108c502a90ca2c9944216dbd4e50a62f206
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-07-22 03:27:47 -0400

    Changed sed command to process file copy string differently.
    Changed sed and cut commands in the script to take the second part
    of a 'file copy' string - i.e the part after the semicolon.

--- extract_files.sh
+++ extract_files.sh
@@ -7,14 +7,15 @@ RED='\033[1;31m'
 NC='\033[0m' # No Color
 
 #get the list of files from the blob file
-FILE_LIST=`grep ':[A-Za-z]' $BLOB_FILE | cut -d ':' -f 1 | sed s/\$\(PROPRIETARY_PATH\)//g | sed s/[#\ ]//g`
+FILE_LIST=`grep ':[A-Za-z]' $BLOB_FILE | cut -d ':' -f 2 | sed s/[\\\]//g | sed s/system/\\\/system/g`
 
 #create the proprietary dir
 mkdir $PROP_DIR -p
 
 for i in $FILE_LIST; do
   echo -e ${RED} "Copying $i ... " ${NC}
-  adb pull $i $PROP_DIR/$i
+  OUT_PATH=`echo $i | sed s/\\\system//g`
+  adb pull $i $PROP_DIR/$OUT_PATH
 done
 
   echo -e ${RED} "Done."
