483266fcdbff7f1bc6bea88effc875fdf19025c3
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-07-22 04:36:22 -0400

    Fixed problematic regexp.
    File names with 'system' in the name were getting clobbered by the
    regexp in the for loop previously so I reverted cut to take the
    first field of the 'file copy' path.

--- extract_files.sh
+++ extract_files.sh
@@ -8,15 +8,13 @@ BLUE='\033[1;35m'
 NC='\033[0m' # No Color
 
 #get the list of files from the blob file
-FILE_LIST=`grep ':[A-Za-z]' $BLOB_FILE | cut -d ':' -f 2 | sed s/[\\\]//g | sed s/system/\\\/system/g`
-
+FILE_LIST=`grep ':[A-Za-z]' $BLOB_FILE | cut -d ':' -f 1 | sed s/\$\(PROPRIETARY_PATH\)//g | sed s/[#\ ]//g`
 #create the proprietary dir
 mkdir $PROP_DIR -p
 
 for i in $FILE_LIST; do
-  OUT_PATH=`echo $i | sed s/\\\/system//g`
-  echo -e ${RED} "Copying ${BLUE}adb:/$i ... to $PROP_DIR$OUT_PATH " ${NC}
-  adb pull $i $PROP_DIR/$OUT_PATH
+  echo -e ${RED} "Copying ${BLUE}adb:/system/$i ... to $PROP_DIR$i " ${NC}
+  adb pull /system/$i $PROP_DIR/$i
 done
 
   echo -e ${RED} "Done."
