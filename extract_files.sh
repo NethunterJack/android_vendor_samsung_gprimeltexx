ea011117fbcb80ea130c6921c7c4011b8f64159b
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-07-22 03:06:14 -0400

    Added shell script for extracting proprietary files.

---
+++ extract_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BLOB_FILE=gprimeltecan-vendor-blobs.mk
+PROP_DIR=proprietary
+
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+#get the list of files from the blob file
+FILE_LIST=`grep ':[A-Za-z]' $BLOB_FILE | cut -d ':' -f 1 | sed s/\$\(PROPRIETARY_PATH\)//g | sed s/[\\#\ ]//g`
+
+#create the proprietary dir
+mkdir $PROP_DIR -p
+
+for i in $FILE_LIST; do
+  echo -e ${RED} "Copying $i ... " ${NC}
+  adb pull $i $PROP_DIR/$i
+done
+
+  echo -e ${RED} "Done."
